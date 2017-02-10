0023f8b2a50733d5027429daf7ca1315fdf9d020
yuki <123syokutaku@gmail.com>
2017-02-10 20:48:25 +0900

    型ヒント削除

--- JPGPainter.py
+++ JPGPainter.py
@@ -30,12 +30,12 @@ class Painter(object):
 class JPGPainter(Painter):
     """ PillowのImage用のPainter """
 
-    def __init__(self, img: Image.Image=None) -> None:
+    def __init__(self, img):
         super().__init__()
         self.canvas = img
         self.drawer = ImageDraw.Draw(img)
 
-    def put_pixel(self, point: Point) -> None:
+    def put_pixel(self, point):
         """ canvasにpointを描画する """
         if 0 <= point.x < self.canvas.width and 0 <= point.y < self.canvas.height:
             self.canvas.putpixel((int(point.x), int(point.y)),
