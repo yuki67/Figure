b6e3dc1724fc0e7e452e993037f9210c38761f1b
yuki <123syokutaku@gmail.com>
2017-02-16 20:37:27 +0900

    ハッシュを使わないように変更

--- JPGPainter.py
+++ JPGPainter.py
@@ -2,35 +2,13 @@ from PIL import ImageDraw
 from Figure import _Point
 
 
-class Painter(object):
-    """ 絵を描くための基底クラス """
-
-    def __init__(self):
-        # draw_functionsで描画に使う関数を指定する
-        # 何も指定しなければ、すべて点で描画される(遅い)
-        self.draw_functions = {
-            _Point: self.put_pixel
-        }
-
-    def put_pixel(self, point):
-        """ canvasにpointを描画する """
-        pass
-
-    def split_and_draw(self, figure):
-        """ figureを分解して描く """
-        for sub_figure in figure:
-            self.draw(sub_figure)
-
-    def draw(self, figure):
-        """ canvasにfigureを描く """
-        self.draw_functions.get(type(figure), self.split_and_draw)(figure)
-
-
-class JPGPainter(Painter):
+class JPGPainter(object):
     """ PillowのImage用のPainter """
 
     def __init__(self, img):
-        super().__init__()
+        self.draw_functions = {
+            _Point: self.put_pixel
+        }
         self.canvas = img
         self.drawer = ImageDraw.Draw(img)
 
@@ -39,3 +17,15 @@ class JPGPainter(Painter):
         if 0 <= point.x < self.canvas.width and 0 <= point.y < self.canvas.height:
             self.canvas.putpixel((int(point.x), int(point.y)),
                                  tuple(point.rgb))
+
+    def split_and_draw(self, figure):
+        """ figureを分解して描く """
+        for sub_figure in figure:
+            self.draw(sub_figure)
+
+    def draw(self, figure):
+        """ canvasにfigureを描く """
+        if isinstance(figure, _Point):
+            self.put_pixel(figure)
+        else:
+            self.split_and_draw(figure)
