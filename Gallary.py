93777029b8775fd4990cab1c539a28013994b4b3
yuki <123syokutaku@gmail.com>
2017-02-17 23:28:45 +0900

    サークロイドをいくつか追加

--- Gallary.py
+++ Gallary.py
@@ -7,29 +7,72 @@ from JPGPainter import JPGPainter
 from MyMatrix import Matrix
 
 
-class Diamond(object):
-    """ ダイヤモンドパターン """
+class Circloid(object):
+    """ サークロイド(造語) """
 
-    def __init__(self, circle, n=16):
+    def __init__(self, circle, n, f):
         self.circle = circle
         self.n = n
+        self.f = f
 
     def __iter__(self):
         points = self.circle.points(self.n)
-        return (Line(p, q) for p in points for q in points)
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.f(i, j):
+                    yield Line(points[i], points[j])
 
 
-class Cardioid(object):
+class Diamond(Circloid):
+    """ ダイヤモンドパターン """
+
+    def __init__(self, circle, n=16):
+        super().__init__(circle, n, lambda i, j: True)
+
+
+class Cardioid(Circloid):
     """ カージオイド """
 
-    def __init__(self, circle, n=128):
-        self.circle = circle
-        self.n = n
+    def __init__(self, circle, n=256):
+        super().__init__(circle, n, lambda i, j: (2 * i) % n == j)
 
-    def __iter__(self):
-        points = list(self.circle.points(self.n))
-        n = len(points)
-        return (Line(points[i], points[(2 * i) % n]) for i in range(n))
+
+class Thirdioid(Circloid):
+    """ サージオイド(造語) """
+
+    def __init__(self, circle, n=256):
+        super().__init__(circle, n, lambda i, j: (3 * i) % n == j)
+
+
+class Waves(Circloid):
+    """ 波っぽいのが見える """
+
+    def __init__(self, circle, n=256):
+        super().__init__(circle, n, lambda i, j: i % (j + 1) == 0)
+
+
+class PineCone(Circloid):
+    """ 松ぼっくりに見えなくもない """
+
+    def __init__(self, circle, n=32):
+        def f(i, j):
+            k = self.n**0.5
+            if -k < (j + i) % self.n < k or (j + i) % self.n > -k % self.n:
+                return True
+            return False
+        super().__init__(circle, n, f)
+
+
+class JumpRope(Circloid):
+    """ 縄跳びをスローシャッターで撮ったときの模様に似てる """
+
+    def __init__(self, circle, n=256):
+        def f(i, j):
+            k = self.n**0.25
+            if -k < (j + i) % self.n < k or (j + i) % self.n > -k % self.n:
+                return True
+            return False
+        super().__init__(circle, n, f)
 
 
 def demo():
@@ -38,8 +81,12 @@ def demo():
 
     circle = Circle(Point(0.5, 0.5), 0.5)
     exhibits = [
-        [Diamond, [circle, 64]],
-        [Cardioid, [circle, 512]]
+        [Diamond, [circle, 32]],
+        [Cardioid, [circle, 256]],
+        [Thirdioid, [circle, 256]],
+        [Waves, [circle, 256]],
+        [PineCone, [circle, 128]],
+        [JumpRope, [circle, 256]],
     ]
 
     for exhibit, args in exhibits:
