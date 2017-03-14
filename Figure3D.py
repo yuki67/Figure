9520e0aa7e3f5e3fd5b796e1501756b11426346c
unknown <>
2017-03-14 21:16:46 +0900

    BoxとGridをFigure3Dに移動
    単純な図形なため

--- Figure3D.py
+++ Figure3D.py
@@ -1,5 +1,5 @@
 """ 2次元の図形 """
-from Figure import Point
+from Figure import Figure, Point, Line
 
 
 class Point3D(Point):
@@ -8,3 +8,50 @@ class Point3D(Point):
     def __init__(self, lst):
         assert len(lst) == 3
         super().__init__(lst + [1.0])
+
+
+class Grid(Figure):
+    """ 四角形内部のグリッド """
+
+    def __init__(self, poly, n1, n2):
+        super().__init__(4)
+        assert len(poly.points) == 4
+        self.poly = poly
+        # グリッドの分割数
+        self.n1 = n1
+        self.n2 = n2
+
+    def get_iter(self):
+        points = self.poly.points
+
+        def gen():
+            for p, q in zip(Line(points[0], points[1], self.n1), Line(points[3], points[2], self.n1)):
+                yield Line(p, q)
+            for p, q in zip(Line(points[1], points[2], self.n2), Line(points[0], points[3], self.n2)):
+                yield Line(p, q)
+        return gen()
+
+
+class Box(Figure):
+    """ 直方体 """
+
+    def __init__(self, a, b):
+        super().__init__(4)
+        self.a = a
+        self.b = b
+
+    def __iter__(self):
+        x, y, z, _ = self.a
+        s, t, u, _ = self.b
+        return iter([Line(self.a, Point3D([x, y, u])),
+                     Line(self.a, Point3D([x, t, z])),
+                     Line(self.a, Point3D([s, y, z])),
+                     Line(self.b, Point3D([s, t, z])),
+                     Line(self.b, Point3D([s, y, u])),
+                     Line(self.b, Point3D([x, t, u])),
+                     Line(Point3D([x, t, z]), Point3D([s, t, z])),
+                     Line(Point3D([s, y, z]), Point3D([s, t, z])),
+                     Line(Point3D([s, y, z]), Point3D([s, y, u])),
+                     Line(Point3D([x, t, z]), Point3D([x, t, u])),
+                     Line(Point3D([x, t, u]), Point3D([x, y, u])),
+                     Line(Point3D([x, y, u]), Point3D([s, y, u]))])
--- Gallery3D.py
+++ Gallery3D.py
@@ -1,57 +1,10 @@
 """ 図形描画のテスト """
 import os
 from PIL import Image
-from Figure import Figure, Line, Polygon
-from Figure3D import Point3D
+from Figure import Polygon
+from Figure3D import Point3D, Grid, Box
 from RendererJPG import RendererJPG3D
 from MyMatrix import Matrix
-
-
-class Grid(Figure):
-    """ 四角形内部のグリッド """
-
-    def __init__(self, poly, n1, n2):
-        super().__init__(4)
-        assert len(poly.points) == 4
-        self.poly = poly
-        # グリッドの分割数
-        self.n1 = n1
-        self.n2 = n2
-
-    def get_iter(self):
-        points = self.poly.points
-
-        def gen():
-            for p, q in zip(Line(points[0], points[1], self.n1), Line(points[3], points[2], self.n1)):
-                yield Line(p, q)
-            for p, q in zip(Line(points[1], points[2], self.n2), Line(points[0], points[3], self.n2)):
-                yield Line(p, q)
-        return gen()
-
-
-class Box(Figure):
-    """ 直方体 """
-
-    def __init__(self, a, b):
-        super().__init__(4)
-        self.a = a
-        self.b = b
-
-    def __iter__(self):
-        x, y, z, _ = self.a
-        s, t, u, _ = self.b
-        return iter([Line(self.a, Point3D([x, y, u])),
-                     Line(self.a, Point3D([x, t, z])),
-                     Line(self.a, Point3D([s, y, z])),
-                     Line(self.b, Point3D([s, t, z])),
-                     Line(self.b, Point3D([s, y, u])),
-                     Line(self.b, Point3D([x, t, u])),
-                     Line(Point3D([x, t, z]), Point3D([s, t, z])),
-                     Line(Point3D([s, y, z]), Point3D([s, t, z])),
-                     Line(Point3D([s, y, z]), Point3D([s, y, u])),
-                     Line(Point3D([x, t, z]), Point3D([x, t, u])),
-                     Line(Point3D([x, t, u]), Point3D([x, y, u])),
-                     Line(Point3D([x, y, u]), Point3D([s, y, u]))])
 
 
 def demo():
