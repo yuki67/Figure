7bac3355f48d26ac81430969baf95391eb1a14a6
yuki <123syokutaku@gmail.com>
2017-03-15 01:59:14 +0900

    BoxGrid追加

--- Gallery3D.py
+++ Gallery3D.py
@@ -16,10 +16,18 @@ class BoxLined(Repeats):
                          n)
 
 
+class BoxGrid(Repeats):
+
+    def __init__(self, a, b, n):
+        super().__init__(BoxLined(a, b, n).transform(Matrix.affine3D(a[:3], scale=(1.0, 1 / n, 1.0))),
+                         Matrix.affine3D(trans=(0.0, (b[1] - a[1]) / n, 0.0)),
+                         n)
+
+
 def demo():
     width, height = 1024, 1024
-    a = Point3D([-10.0, -2.5, 10.0])
-    b = Point3D([10.0, 2.5, 15.0])
+    a = Point3D([-10.0, -10.0, 10.0])
+    b = Point3D([10.0, 10.0, 15.0])
     poly = Polygon([Point3D([-5.0, 0.0, 6.0]),
                     Point3D([5.0, 0.0, 6.0]),
                     Point3D([5.0, 0.0, 15.0]),
@@ -29,11 +37,12 @@ def demo():
         [Grid, (poly, 15, 15)],
         [Box, (a, b)],
         [BoxLined, (a, b, 5)],
+        [BoxGrid, (a, b, 5)],
     ]
 
     for exhibit, args in exhibits:
         img = Image.new("RGB", (width + 1, height + 1), "white")
-        renderer = RendererJPG3D(img, Matrix.affine3D(trans=(0.0, 0.0, 0.0)))
+        renderer = RendererJPG3D(img, Matrix.affine3D())
 
         figure = exhibit(*args)
         print("%s begin." % figure.__class__.__name__)
