d5d9040a6ec288786c68038ca8b4d76f4dc94841
yuki <123syokutaku@gmail.com>
2017-03-15 01:58:17 +0900

    レンダラーの掛け算忘れ修正

--- Renderer.py
+++ Renderer.py
@@ -7,7 +7,7 @@ class Renderer(object):
     def render(self, figure):
         """ figureの座標を変換して描画関数に渡す """
         if self.render_functions.get(type(figure)):
-            self.render_functions[type(figure)](figure.transformed(self.screen_mat))
+            self.render_functions[type(figure)](figure.transformed(figure.mat * self.screen_mat))
         else:
             for sub_figure in figure:
                 self.render(sub_figure)
