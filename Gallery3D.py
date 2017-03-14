f02c8f71a35250ac0d8e9c23858fe395e68efb79
yuki <123syokutaku@gmail.com>
2017-03-14 23:02:43 +0900

    BoxLinedを追加

--- Gallery3D.py
+++ Gallery3D.py
@@ -1,15 +1,25 @@
 """ 図形描画のテスト """
 import os
 from PIL import Image
-from Figure import Polygon
+from Figure import Polygon, Repeats
 from Figure3D import Point3D, Grid, Box
 from RendererJPG import RendererJPG3D
 from MyMatrix import Matrix
 
 
+class BoxLined(Repeats):
+
+    def __init__(self, a, b, n):
+        box = Box(a, b)
+        super().__init__(box.transform(Matrix.affine3D(center=box.a[:3], scale=(1 / n, 1.0, 1.0))),
+                         Matrix.affine3D(trans=((b[0] - a[0]) / n, 0.0, 0.0)),
+                         n)
+
+
 def demo():
     width, height = 1024, 1024
-
+    a = Point3D([-10.0, -2.5, 10.0])
+    b = Point3D([10.0, 2.5, 15.0])
     poly = Polygon([Point3D([-5.0, 0.0, 6.0]),
                     Point3D([5.0, 0.0, 6.0]),
                     Point3D([5.0, 0.0, 15.0]),
@@ -17,12 +27,13 @@ def demo():
 
     exhibits = [
         [Grid, (poly, 15, 15)],
-        [Box, (Point3D([-5.0, -5.0, 10.0]), Point3D([5.0, 5.0, 15.0]))]
+        [Box, (a, b)],
+        [BoxLined, (a, b, 5)],
     ]
 
     for exhibit, args in exhibits:
         img = Image.new("RGB", (width + 1, height + 1), "white")
-        renderer = RendererJPG3D(img, Matrix.affine3D(trans=(0.1, 3.0, 0.1)))
+        renderer = RendererJPG3D(img, Matrix.affine3D(trans=(0.0, 0.0, 0.0)))
 
         figure = exhibit(*args)
         print("%s begin." % figure.__class__.__name__)
