8c34b552dd3d1854eb8dfd1cfd3ff9534531dc6b
yuki <123syokutaku@gmail.com>
2017-04-08 23:35:48 +0900

    Mountainを簡略化

--- Gallery2D/Mountain.py
+++ Gallery2D/Mountain.py
@@ -18,10 +18,7 @@ class Mountain(Figure):
         t = (2 * d * random.random()**2 - d) * dy
         center = sum([p for p in self.poly.points], point_2d(0, 0)).scaled(1 / 4) + point_2d(r, t)
         mid_points = [(self.poly.points[i - 1] + self.poly.points[i]).scaled(1 / 2) for i in range(4)]
-        return iter((Mountain(Polygon([self.poly.points[0], mid_points[0], center, mid_points[1]]), self.n - 1),
-                     Mountain(Polygon([self.poly.points[1], mid_points[1], center, mid_points[2]]), self.n - 1),
-                     Mountain(Polygon([self.poly.points[2], mid_points[2], center, mid_points[3]]), self.n - 1),
-                     Mountain(Polygon([self.poly.points[3], mid_points[3], center, mid_points[0]]), self.n - 1)))
+        return (Mountain(Polygon([self.poly.points[i - 1], mid_points[i - 1], center, mid_points[i]]), self.n - 1) for i in range(4))
 
     def __init__(self, poly, n):
         super().__init__(3)
